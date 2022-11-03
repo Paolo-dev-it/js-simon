7242ee506d18cc759298885fa30f29a558566806
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-03 17:07:29 +0100

    Small adjustment, added comments. Project completed!

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,12 @@
 // Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-let arrayNumbers = [];
-let arrayNumbersUser = [];
-let numbersCorrect = [];
+let arrayNumbers = []; //Array vuoto con i numeri random
+let arrayNumbersUser = []; //Array vuoto con i numeri inseriti dall'utente
+let numbersCorrect = []; //Array vuoto con i numeri corretti
 let correct = document.getElementById('correct');
 
-for(let i = 0; i < 5; i++){
+for(let i = 0; i < 5; i++){ // Ciclo che crea 5 numeri random e li pusha all'interno nell'array vuoto
     
     let numberCasual = randomNumber();
 
@@ -22,59 +22,51 @@ for(let i = 0; i < 5; i++){
 }
 
 
-document.getElementById('numbers').innerHTML = `${arrayNumbers}`;
+document.getElementById('numbers').innerHTML = `${arrayNumbers}`; //Scrivo sul dom i numeri nell'array
 
-setTimeout( function(){
+setTimeout( function(){ // Funzione timeout, dopo 5 secondi fa scomparire i numeri
     hideElements();
-    
 }, 5000 );
 
-setTimeout( function(){
-    functionTime();
-    
+setTimeout( function(){ // Funzione timeout, dopo 5,1 secondi richiama la funzione con i prompt
+    functionTime(); 
 }, 5100 );
 
 
 function hideElements(){
-     document.getElementById('numbers').innerHTML = '';
-    
+     document.getElementById('numbers').innerHTML = ''; 
 }
 
-function functionTime(){
-
+function functionTime(){ //Funzione che crea i 5 prompt dove l'utente inserisce i suoi numeri
     for(let n = 0; n < 5; n++){
         let time = parseInt(prompt('Scrivi il numero, se te lo ricordi ;)'))
         arrayNumbersUser.push(time)    
     }
     document.getElementById('counter').innerHTML = `${arrayNumbersUser}`;
+
     check();
 }
 
-// if (arrayNumbers.value !== arrayNumbersUser.value){
-//     document.getElementById('result').innerHTML = "Hai perso";
-// } else {
-//     document.getElementById('result').innerHTML = "Hai vinto";
-// }
 
-function check(){
-    for (let x = 0; x < arrayNumbers.length; x++){
+function check(){ //Creo la funzione di controllo dei numeri
+    for (let x = 0; x < arrayNumbers.length; x++){ //Ciclo che controlla per la lunghezza dell'array 
 
-        for (let k = 0; k < arrayNumbersUser.length; k++){
+        for (let k = 0; k < arrayNumbersUser.length; k++){//Ciclo che controlla se i numeri all'interno degli array sono uguali
 
-         if(arrayNumbers[x] == arrayNumbersUser[k]){
+         if(arrayNumbers[x] == arrayNumbersUser[k]){ //Se i valori dell'array con i numeri random e i valori dell'array dell'utente sono uguali
 
-            numbersCorrect.push(arrayNumbersUser[k]);
-            console.log(numbersCorrect);
+            numbersCorrect.push(arrayNumbersUser[k]); //Li pusha all'interno dell'array corretto vuoto
+            
 
          }
 
       }
 
     }
+console.log(numbersCorrect);
+    let controlCorrect = numbersCorrect.length; //Variabile per la lunghezza dell'array con i numeri corretti
 
-    let controlCorrect = numbersCorrect.length;
-
-    correct.innerHTML = `Hai inserito ${controlCorrect} numeri corretti su 5: ${numbersCorrect}`
+    correct.innerHTML = `Hai inserito ${controlCorrect} numeri corretti su 5: ${numbersCorrect}` 
 
 }
 
